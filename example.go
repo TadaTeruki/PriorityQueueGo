0fc755b51f087fd0f224a804a7f71ba7930c4890
TadaTeruki <tadateruki.public@gmail.com>
2022-01-24 20:22:42 +0900

    Annotated all lines of example.go

--- example.go
+++ example.go
@@ -3,20 +3,32 @@ package main
 
 import(
 	"fmt"
-	priority_queue "./PriorityQueue"
+	priority_queue "github.com/TadaTeruki/PriorityQueueGo/PriorityQueue"
 )
 
 func main(){
 	var que priority_queue.PriorityQueue
-	
+
+	// 'MakeObject(value interface{}, priority float64) -> Object'
+	//    - create a new object
+	// '(PriorityQueue).Push(Object)'
+	//    - Push object to PriorityQueue
 	que.Push(priority_queue.MakeObject("1st", 0.5))
 	que.Push(priority_queue.MakeObject("2nd", 2.5))
 	que.Push(priority_queue.MakeObject("3rd", 8.5))
 	que.Push(priority_queue.MakeObject("4th", 4.5))
 
+	// '(PriorityQueue).GetSize()'
+	//    - Query the number of objects
 	fmt.Println(que.GetSize()) 								   // -> 4
+
+	// '(PriorityQueue).GetFront()'
+	//    - Query the object which has the highest priority
 	fmt.Println(que.GetFront().Value, que.GetFront().Priority) // -> "3th", 8.5
 
+	// '(PriorityQueue).PopFront()'
+	//    - Remove the object which has the highest priority
 	que.PopFront()
+	
 	fmt.Println(que.GetFront().Value, que.GetFront().Priority) // -> "4th", 4.5
 }
